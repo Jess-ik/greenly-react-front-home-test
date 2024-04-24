d7684f77119f50b84d91cb12b3d4ff86955a38cc
Jess <jessica.louvel@gmail.com>
2024-04-24 12:36:48 +0200

    Handle tags selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,72 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
 import Recipes from "./components/Recipes/Recipes";
 import { allRecipes, sugarRecipes, summerRecipes, dessertRecipes, noEggsRecipes, chocolateRecipes, autumnRecipes, veganRecipes, chocolateDessertRecipes } from "./data/RecipeData";
 
 import { tagList } from "./data/TagData";
 import { Tags } from "./components/Tags/Tags";
+import { Tag } from "./components/Tags/Tag";
+import { TagType } from "./types/TagType";
 
 export default function App() {
-  const [filter, setFilter] = useState<string>("all");
-  
-  // Store selected tags
-  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+	const [filter, setFilter] = useState<string>("all");
 
-  // Handle storing/removing tags on click
-  const handleClick = () => {
+	// Store selected tags
+	const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+	// Handle storing/removing tags on click
+  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, tag: TagType) => {
     // if tag already stored, remove it
-    // else add it
-  }
+    if (selectedTags.includes(tag.id)) {
+      setSelectedTags(prevTags => prevTags.filter(item => item !== tag.id));
+    } // else add it
+    else {
+      setSelectedTags(prevTags => [...prevTags, tag.id]);
+    }	
+  };
+  
+  useEffect(() => {
+    console.log("Voici les tags sélectionnés : " + selectedTags);
+  }, [selectedTags]);
 
-  // Handle recipes filtering
-  const filteredRecipes = () => {
-    // if selectedTags empty, show all recipes
-    // else, show recipes that contains every selected tags
-  }
+	// Handle recipes filtering
+	const filteredRecipes = () => {
+		// if selectedTags empty, show all recipes
+		// else, show recipes that contains every selected tags
+	};
 
 	return (
 		<div className="App">
 			Liste des recettes
-			<Tags tags={tagList} />
-			{/* <>
+			{/* <Tags tags={tagList} /> */}
+			<div
+				style={{
+					margin: "10px",
+					padding: "5px",
+					borderRadius: "3px",
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "center",
+				}}>
+				{tagList.map((tag) => (
+					<div
+						style={{
+							margin: "10px",
+							padding: "5px",
+							borderRadius: "3px",
+							display: "flex",
+							flexDirection: "row",
+							justifyContent: "center",
+							backgroundColor: `${tag.color}`,
+            }}
+            onClick={(event) => handleClick(event, tag)}
+>
+						{tag.name.fr}
+					</div>
+				))}
+			</div>
+			{/* 
+      <>
 				{" "}
 				<button onClick={() => setFilter("chocolate")}>Chocolat</button>
 				<button onClick={() => setFilter("sugar")}>Sucre</button>
@@ -47,9 +85,9 @@ export default function App() {
 			{filter === "chocolate" && <Recipes recipes={chocolateRecipes} />}
 			{filter === "autumn" && <Recipes recipes={autumnRecipes} />}
       {filter === "vegan" && <Recipes recipes={veganRecipes} />}
-			</> */}
-			
-      <Recipes recipes={allRecipes} />
+			</> 
+      */}
+			<Recipes recipes={allRecipes} />
 		</div>
 	);
 }
