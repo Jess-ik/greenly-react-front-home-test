21cc7d0a39b83b189868b875d57ee2d9b9ee61a7
Jess <jessica.louvel@gmail.com>
2024-04-24 11:44:43 +0200

    Setup filtering logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,55 @@
 import { useState } from "react";
 import "./styles.css";
 import Recipes from "./components/Recipes/Recipes";
-import {
-  allRecipes,
-  sugarRecipes,
-  summerRecipes,
-  dessertRecipes,
-  noEggsRecipes,
-  chocolateRecipes,
-  autumnRecipes,
-  veganRecipes,
-  chocolateDessertRecipes,
-} from "./data/RecipeData";
+import { allRecipes, sugarRecipes, summerRecipes, dessertRecipes, noEggsRecipes, chocolateRecipes, autumnRecipes, veganRecipes, chocolateDessertRecipes } from "./data/RecipeData";
 
 import { tagList } from "./data/TagData";
 import { Tags } from "./components/Tags/Tags";
 
 export default function App() {
   const [filter, setFilter] = useState<string>("all");
-  return (
-    <div className="App">
-      Liste des recettes
-      
-      <Tags tags={tagList} />
-      <button onClick={() => setFilter("chocolate")}>Chocolat</button>
-      <button onClick={() => setFilter("sugar")}>Sucre</button>
-      <button onClick={() => setFilter("summer")}>Eté</button>
-      <button onClick={() => setFilter("dessert")}>Dessert</button>
-      <button onClick={() => setFilter("chocolate dessert")}>
-        dessert chocolat
-      </button>
-      <button onClick={() => setFilter("eggs free")}>Sans Oeufs</button>
-      <button onClick={() => setFilter("autumn")}> Autumn</button>
-      <button onClick={() => setFilter("vegan")}> Vegan</button>
-      {filter === "all" && <Recipes recipes={allRecipes} />}
-      {filter === "sugar" && <Recipes recipes={sugarRecipes} />}
-      {filter === "summer" && <Recipes recipes={summerRecipes} />}
-      {filter === "dessert" && <Recipes recipes={dessertRecipes} />}
-      {filter === "chocolate dessert" && (
-        <Recipes recipes={chocolateDessertRecipes} />
-      )}
-      {filter === "eggs free" && <Recipes recipes={noEggsRecipes} />}
-      {filter === "chocolate" && <Recipes recipes={chocolateRecipes} />}
-      {filter === "autumn" && <Recipes recipes={autumnRecipes} />}
+  
+  // Store selected tags
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  // Handle storing/removing tags on click
+  const handleClick = () => {
+    // if tag already stored, remove it
+    // else add it
+  }
+
+  // Handle recipes filtering
+  const filteredRecipes = () => {
+    // if selectedTags empty, show all recipes
+    // else, show recipes that contains every selected tags
+  }
+
+	return (
+		<div className="App">
+			Liste des recettes
+			<Tags tags={tagList} />
+			{/* <>
+				{" "}
+				<button onClick={() => setFilter("chocolate")}>Chocolat</button>
+				<button onClick={() => setFilter("sugar")}>Sucre</button>
+				<button onClick={() => setFilter("summer")}>Eté</button>
+				<button onClick={() => setFilter("dessert")}>Dessert</button>
+				<button onClick={() => setFilter("chocolate dessert")}>dessert chocolat</button>
+				<button onClick={() => setFilter("eggs free")}>Sans Oeufs</button>
+				<button onClick={() => setFilter("autumn")}> Autumn</button>
+        <button onClick={() => setFilter("vegan")}> Vegan</button>
+        {filter === "all" && <Recipes recipes={allRecipes} />}
+			{filter === "sugar" && <Recipes recipes={sugarRecipes} />}
+			{filter === "summer" && <Recipes recipes={summerRecipes} />}
+			{filter === "dessert" && <Recipes recipes={dessertRecipes} />}
+			{filter === "chocolate dessert" && <Recipes recipes={chocolateDessertRecipes} />}
+			{filter === "eggs free" && <Recipes recipes={noEggsRecipes} />}
+			{filter === "chocolate" && <Recipes recipes={chocolateRecipes} />}
+			{filter === "autumn" && <Recipes recipes={autumnRecipes} />}
       {filter === "vegan" && <Recipes recipes={veganRecipes} />}
-    </div>
-  );
+			</> */}
+			
+      <Recipes recipes={allRecipes} />
+		</div>
+	);
 }
