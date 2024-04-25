f1e351b5feb45e551605937ec774407b6f64b382
Jess <jessica.louvel@gmail.com>
2024-04-25 11:43:16 +0200

    Style Recipe Steps

--- src/components/Steps/Step.tsx
+++ src/components/Steps/Step.tsx
@@ -1,34 +1,18 @@
+import "./Steps.css"
 import IngredientsWithQuantity from "../Ingredients/IngredientsWithQuantity";
 import { StepType } from "../../types/StepType";
 
 export const Step = ({ etape }: { etape: StepType }) => {
-  return (
-    <div
-      style={{
-        backgroundColor: "thistle",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        textAlign: "left",
-        padding: "10px",
-      }}
-    >
-      <div>
-        {etape.order}. {etape.description}
-      </div>
-      <div
-        style={{
-          padding: "10px",
-        }}
-      >
-        {etape.ingredients && (
-          <IngredientsWithQuantity
-            ingredientsWithQuantity={etape.ingredients}
-          />
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="step-description">
+				{etape.order}. {etape.description}
+			</div>
+			<div className="step-ingredients">
+				{etape.ingredients && <IngredientsWithQuantity ingredientsWithQuantity={etape.ingredients} />}
+			</div>
+		</div>
+	);
 };
 
 export default Step;
